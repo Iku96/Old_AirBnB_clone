740edb13fa5c7d13ea9e37733f15c915c6500c1f
Iku96 <ikumwana@gmail.com>
2023-05-29 10:15:08 -0700

    Added missing test files, updated console.py, updated base_model.py, anf files_storage.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,61 +1,51 @@
 #!/usr/bin/python3
+"""Defines the BaseModel class."""
+import models
+from uuid import uuid4
 from datetime import datetime
-import uuid
-from models import storage
+
 
 class BaseModel:
-    """
-    Base class for other classes in the project.
-
-    Public instance attributes:
-        id: string - assigned with a unique UUID when an instance is created.
-            The UUID is generated using the uuid.uuid4() function and then converted to a string.
-        created_at: datetime - assigned with the current datetime when an instance is created.
-        updated_at: datetime - assigned with the current datetime when an instance is created
-            and updated every time the object is changed.
-    """
+    """Represents the BaseModel of the HBnB project."""
 
     def __init__(self, *args, **kwargs):
-        """
-        Initialize a new instance of BaseModel.
+        """Initialize a new BaseModel.
 
         Args:
-            *args: Variable length argument list (not used in this implementation).
-            **kwargs: Arbitrary keyword arguments.
-                Each key of the dictionary represents an attribute name, and the corresponding value
-                is the value for that attribute.
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
         """
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != "__class__":
-                    setattr(self, key, value)
-                if "created_at" in kwargs:
-                    self.created_at = datetime.strptime(kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                if "updated_at" in kwargs:
-                    self.updated_at = datetime.strptime(kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, tform)
+                else:
+                    self.__dict__[k] = v
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
 
     def save(self):
-        """Save the instance to the storage"""
-        self.updated_at = datetime.now()
-        storage.save()
-
+        """Update updated_at with the current datetime."""
+        self.updated_at = datetime.today()
+        models.storage.save()
 
     def to_dict(self):
-        """
-        Return a dictionary representation of the object.
+        """Return the dictionary of the BaseModel instance.
 
-        Returns:
-            dict: A dictionary containing all keys/values of the instance's attributes.
-                The dictionary also includes the '__class__' key with the class name of the object,
-                and the 'created_at' and 'updated_at' keys with their corresponding values in ISO format.
+        Includes the key/value pair __class__ representing
+        the class name of the object.
         """
-        obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
-        return obj_dict
+        rdict = self.__dict__.copy()
+        rdict["created_at"] = self.created_at.isoformat()
+        rdict["updated_at"] = self.updated_at.isoformat()
+        rdict["__class__"] = self.__class__.__name__
+        return rdict
+
+    def __str__(self):
+        """Return the print/str representation of the BaseModel instance."""
+        clname = self.__class__.__name__
+        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
