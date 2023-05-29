4fddfe5b6dadcc6355c3bbebb3de8b461d80d7c7
Iku96 <ikumwana@gmail.com>
2023-05-29 12:46:14 -0700

    Fixed errors in test_file_storage.py and file_storage.py

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -6,18 +6,17 @@ Unittest classes:
     TestFileStorage_methods
 """
 import os
-import json
-import models
 import unittest
-from datetime import datetime
-from models.base_model import BaseModel
-from models.engine.file_storage import FileStorage
-from models.user import User
-from models.state import State
-from models.place import Place
-from models.city import City
+
+import models
 from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.engine.file_storage import FileStorage
+from models.place import Place
 from models.review import Review
+from models.state import State
+from models.user import User
 
 
 class TestFileStorage_instantiation(unittest.TestCase):
